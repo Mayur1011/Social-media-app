88eaf9f32b1b1f64dea1f5822af9060c4591e42c
Mayur Pokharkar <mayurpokharkar6@gmail.com>
2022-01-09 00:47:55 +0530

    Added like-dislike feature to a post

--- routes/posts.js
+++ routes/posts.js
@@ -23,7 +23,9 @@ router.put("/:id", async (req, res) => {
 
     // Check if the user updating the post is author/owner of the post
     if (post.userId == req.body.userId) {
-      await post.updateOne({ $set: req.body });
+      await post.updateOne({
+        $set: req.body,
+      });
       res.status(200).json("Post successfully updated!");
     } else {
       res.status(404).json("You can't update the post!");
@@ -52,7 +54,30 @@ router.delete("/:id", async (req, res) => {
 });
 
 // like a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    // Find the post
+    const post = await Post.findById(req.params.id);
+
+    // Check whether the post is already liked by the current user.
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      // Success Message
+      res.status(200).send("Post Liked!");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).send("Post Disliked!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // get a post
+router.get("/:id", async (req, res) => {
+  try {
+  } catch (err) {}
+});
+
 // get timeline posts
 
 module.exports = router;
